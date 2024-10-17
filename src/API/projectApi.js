3c6a3d4bad6e09e3192f006fcb4b95ce431a1429
Chaitanya Sawant <csawant.cs7@gmail.com>
2024-10-17 15:12:18 +0530

    added project

--- src/API/projectApi.js
+++ src/API/projectApi.js
@@ -1,13 +1,21 @@
 const projectapi = [
   {
     id: 1,
+    name: "Fine-tuned Falcon-7B chat model for text generation",
+    description:
+      "This is a fine-tuned  Falcon-7B language model using the LoRA technique and the OpenAssistant Guanaco dataset. The resulting model, Falcon-7b-chat-new-finetune, is optimized for conversational AI tasks and available on Hugging Face for easy integration into your projects. The notebook utilizes techniques like 4-bit quantization.",
+    link: "https://huggingface.co/chaitanya42/Falcon-7b-chat-new-finetune",
+    // date: "January 2024",
+  },
+  {
+    id: 2,
     name: "AI Conversational Assistant with Flask and LangChain",
     description:
       "An AI-powered RAG chatbot with Flask, LangChain, and ChromaDB for real-time interaction and efficient document management. Utilized large language models for natural language understanding and added conversational memory for personalized user experiences.",
     // date: "Oct 2023",
   },
   {
-    id: 2,
+    id: 3,
     name: "TextGuard: Hate Speech Classification System",
     description:
       "This project aims to develop a robust machine learning model using Natural Language Processing (NLP) and Recurrent Neural Networks (RNN) with Long Short-Term Memory (LSTM) to detect and classify hate speech in social media texts.",
@@ -15,7 +23,7 @@ const projectapi = [
     // date: "Oct 2023",
   },
   {
-    id: 3,
+    id: 4,
     name: "GrapeGauge: Wine Quality Predictive Model with MLflow",
     description:
       "Developed a machine learning pipeline using MLflow to predict wine quality, achieving 90% accuracy and significantly enhancing quality assessment procedures. The project uses MLflow which is used for experiment tracking.",
@@ -23,7 +31,7 @@ const projectapi = [
     // date: "November 2023",
   },
   {
-    id: 4,
+    id: 5,
     name: "CineMatch: Movie Recommendation Engine",
     description:
       "Built a content-based movie recommendation system that suggests movies based on user preferences, increasing user engagement and enhancing satisfaction ratings",
@@ -34,6 +42,3 @@ const projectapi = [
 ];
 
 export default projectapi;
-
-
-//  Developed an AI-powered RAG chatbot utilizing Flask for the backend and LangChain to integrate large language models with an interactive user interface, enabling real-time user interaction.Implemented ChromaDB for efficient document storage and retrieval, and incorporated conversational memory to maintain seamless, context-aware conversations
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -23,13 +23,34 @@ const Experience = () => {
                 <li>
                   Developed RESTful APIs for machine learning models using Flask, achieving 95% uptime and ensuring seamless integration with existing applications.
                 </li>
-                <br />
+                {/* <br /> */}
                 <li>
                   Performed exploratory data analysis (EDA) on time series datasets, yielding actionable insights that enhanced feature engineering and model selection, and reduced data preprocessing time by 30%.
                 </li>
-                <br />
+                {/* <br /> */}
                 <li>
                   Designed and implemented a scalable machine learning pipeline, encompassing data preprocessing, feature engineering, model training, and evaluation, which improved deployment efficiency by 40% over previous methodologies.
+                </li>
+              </ul>
+            </p>
+          </div>
+
+          <div className="left-side-head col-12 col-lg-6">
+            <h1 className="edu-head">Data Science Intern</h1>
+            <h3 className="edu-branch">BCG X</h3>
+            <h5 style={{ color: "black" }}>Oct 2024 – Nov 2024</h5>
+          </div>
+          <div className="right-side-head col-12 col-lg-6">
+            <p className="edu-para">
+              <ul>
+                <li>
+                Performed exploratory data analysis (EDA) to identify trends and patterns within large datasets, enhancing data-driven
+                decision-making processes.
+                </li>
+                {/* <br /> */}
+                <li>
+                Developed and implemented feature engineering techniques and predictive modeling approaches, improving the accuracy
+                of the machine learning model.
                 </li>
               </ul>
             </p>
