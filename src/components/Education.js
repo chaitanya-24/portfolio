8ae299cea84d01533ab639895847386a38f516e9
Chaitanya Sawant <csawant.cs7@gmail.com>
2024-12-19 18:12:21 +0530

    updated education section

--- src/components/Education.js
+++ src/components/Education.js
@@ -11,12 +11,14 @@ const Education = () => {
           <div className="left-side-head col-12 col-lg-6">
             <h1 className="edu-head">Master of Science </h1>
             <h3 className="edu-branch">Computer Science</h3>
-            <h5 style={{ color: "black" }}>Aug 2022 - Sep 2024</h5>
+            <h5 style={{ color: "black" }}>2022 - 2024</h5>
           </div>
           <div className="right-side-head col-12 col-lg-6">
             <p className="edu-para">
-              I completed my <b>Master of Computer Science</b> from <br />{" "}
-              <b>G N Khalsa College, Mumbai</b>.
+              <ul>
+                <li>Specialized in <b>Machine Learning</b> and <b>Artificial Intelligence</b>.</li>
+                <li>My studies include relevant coursework in Machine Learning, Deep Learning, Natural Language Processing, and Cloud Computing.</li>
+              </ul>
             </p>
           </div>
         </main>
